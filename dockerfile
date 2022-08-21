9ee95fb0b7f8e27a65da0792822e77fec4e65e0f
Brian Hartford <bhartford419@gmail.com>
2022-08-20 21:00:58 -0400

    moved most of entrypoint into build

--- dockerfile
+++ dockerfile
@@ -10,10 +10,6 @@ RUN apt-get update && \
     net-tools 
 
 
-#ARG UID=1000
-#ARG GID=1000
-#ARG USERNAME=wireguard
-#ARG GROUPNAME=wireguard
 
 ARG UID=1001
 ARG GID=1001
@@ -38,9 +34,27 @@ RUN chmod 777 /usr/local/entrypoint.sh \
 
 RUN sed '/net.ipv4.ip_forward=1/s/^#//' -i /etc/sysctl.conf \
     sysctl -p
-WORKINGDIR umask 077; wg genkey | tee privatekey | wg pubkey > publickey
-   
+WORKDIR /etc/wireguard    
+RUN umask 077; wg genkey | tee privatekey | wg pubkey > publickey \
+    private_key=$(< privatekey)
 
+RUN load_config=" \
+[Interface] \
+PrivateKey = a_private_key \
+Address = 10.0.0.0/24 \
+ListenPort = 51820 \
+PostUp = iptables -A FORWARD -i wg0 -j ACCEPT; iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE \
+PostDown = iptables -D FORWARD -i wg0 -j ACCEPT; iptables -t nat -D POSTROUTING -o eth0 -j MASQUERADE \
+[Peer] \
+AllowedIPS = 10.0.0.1/24 \
+PersistentKeepalive = 25"
+
+RUN echo "$load_config" | tee wg0.conf  \
+    sed -i "s/a_private_key/$private_key/g" /etc/wireguard/wg0.conf
+
+RUN ufw allow 22/tcp \
+    ufw allow 51820/udp \
+    ufw enable
 
 EXPOSE 51820/udp \
     22/tcp
--- entrypoint.sh
+++ entrypoint.sh
@@ -5,43 +5,43 @@
 
 
 #Enable IPv4 forwarding
-sudo sed '/net.ipv4.ip_forward=1/s/^#//' -i /etc/sysctl.conf
-sudo sysctl -p
+#sudo sed '/net.ipv4.ip_forward=1/s/^#//' -i /etc/sysctl.conf
+#sudo sysctl -p
 
 #Create variable for host's public IP
-first_ip_address="$(curl -Ls ifconfig.me)"
+#first_ip_address="$(curl -Ls ifconfig.me)"
 
 echo "Your public IP is: " $first_ip_address
 
-cd /etc/wireguard/
+#cd /etc/wireguard/
 #touch /etc/wireguard/wg0.conf
 
 #Generate public/private keypair 
-umask 077; wg genkey | tee privatekey | wg pubkey > publickey
+#umask 077; wg genkey | tee privatekey | wg pubkey > publickey
 
 #Create variable for private key
-private_key=$(< privatekey)
+#private_key=$(< privatekey)
 
-load_config="
-[Interface]
-PrivateKey = a_private_key
-Address = 10.0.0.0/24
-ListenPort = 51820
-PostUp = iptables -A FORWARD -i wg0 -j ACCEPT; iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
-PostDown = iptables -D FORWARD -i wg0 -j ACCEPT; iptables -t nat -D POSTROUTING -o eth0 -j MASQUERADE
-[Peer]
-AllowedIPS = 10.0.0.1/24
-PersistentKeepalive = 25
-"
+#load_config="
+#[Interface]
+#PrivateKey = a_private_key
+#Address = 10.0.0.0/24
+#ListenPort = 51820
+#PostUp = iptables -A FORWARD -i wg0 -j ACCEPT; iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
+#PostDown = iptables -D FORWARD -i wg0 -j ACCEPT; iptables -t nat -D POSTROUTING -o eth0 -j MASQUERADE
+#[Peer]
+#AllowedIPS = 10.0.0.1/24
+#PersistentKeepalive = 25
+#"
 
 
 #Populate begining of config file
-sudo echo "$load_config" >> /etc/wireguard/wg0.conf
+#sudo echo "$load_config" >> /etc/wireguard/wg0.conf
 
-sleep 2
+#sleep 2
 
 #Sed script to replace string w/ variable
-sudo sed -i "s/a_private_key/$private_key/g" /etc/wireguard/wg0.conf
+#sudo sed -i "s/a_private_key/$private_key/g" /etc/wireguard/wg0.conf
 
 
 #Read user input as variable
@@ -55,8 +55,8 @@ sudo wg-quick up wg0
 
 
 #Adjust firewall to allow SSH and wireguardVPN traffic
-sudo ufw allow 22/tcp
-sudo ufw allow 51820/udp
-sudo ufw enable
+#sudo ufw allow 22/tcp
+#sudo ufw allow 51820/udp
+#sudo ufw enable
 
 su - wireguard
